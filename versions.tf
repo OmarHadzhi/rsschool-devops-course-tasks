6c4875f45520ee2af05f149bf85702d6d969e5b1
OHadzhi <omarhadzhi@gmail.com>
2025-06-14 11:00:31 +0600

    task_1 changes: Run 5

--- versions.tf
+++ versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-tfstate-s3-bucket-1221"
+    bucket       = "my-tfstate-s3-bucket-1221"
     key          = "global/mystatefile/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true

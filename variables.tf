2fa670cc507c407619579f97cd53fc85c3262e24
OHadzhi <omarhadzhi@gmail.com>
2025-06-14 19:57:58 +0600

    task_1 changes: CI/CD Last Update

--- variables.tf
+++ variables.tf
@@ -7,16 +7,23 @@ variable "aws_region" {
 variable "policy_arns" {
   type        = list(string)
   description = "List of IAM policy ARNs for GitHub Actions role"
+  sensitive   = true
 }
 
 variable "github_username" {
-  type = string
+  type        = string
+  sensitive   = true
+  description = "GitHub username for the repository that will use GitHub Actions"
 }
 
 variable "github_repo" {
-  type = string
+  type        = string
+  sensitive   = true
+  description = "GitHub repository name that will use GitHub Actions"
 }
 
 variable "sample_bucket" {
-  type = string
+  type        = string
+  sensitive   = true
+  description = "Name of the S3 bucket to be created for GitHub Actions artifacts"
 }
